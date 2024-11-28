3f8d5a64a0da195df8a5693b20279dad6a1c5d9f
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-27 23:28:52 -0500

    Start working on website

--- app/page.tsx
+++ app/page.tsx
@@ -12,83 +12,92 @@ import {
   DialogStackTrigger,
 } from '@/registry/dialog-stack';
 
-export default function Home() {
-  return (
-    <DialogStack clickable>
-      <DialogStackTrigger>Open</DialogStackTrigger>
-      <DialogStackOverlay />
+const Home = () => (
+  <>
+    <header className="flex justify-center items-center py-12">
+      <h1 className="max-w-2xl text-center font-bold text-3xl leading-tight tracking-[-0.06em] md:text-6xl lg:leading-[1.1]">
+        Beautiful stacked dialogs for shadcn/ui
+      </h1>
+    </header>
+    <div className="bg-secondary">
+      <DialogStack clickable>
+        <DialogStackTrigger>Open</DialogStackTrigger>
+        <DialogStackOverlay />
 
-      <DialogStackBody>
-        <DialogStackContent>
-          <DialogHeader>
-            <DialogStackTitle>Title</DialogStackTitle>
-            <DialogStackDescription>Description</DialogStackDescription>
-          </DialogHeader>
-          <p>Content</p>
-          <DialogStackFooter>
-            <DialogStackNext>Next</DialogStackNext>
-          </DialogStackFooter>
-        </DialogStackContent>
+        <DialogStackBody>
+          <DialogStackContent>
+            <DialogHeader>
+              <DialogStackTitle>Title</DialogStackTitle>
+              <DialogStackDescription>Description</DialogStackDescription>
+            </DialogHeader>
+            <p>Content</p>
+            <DialogStackFooter>
+              <DialogStackNext>Next</DialogStackNext>
+            </DialogStackFooter>
+          </DialogStackContent>
 
-        <DialogStackContent>
-          <DialogHeader>
-            <DialogStackTitle>Title</DialogStackTitle>
-            <DialogStackDescription>Description</DialogStackDescription>
-          </DialogHeader>
-          <p>Content</p>
-          <DialogStackFooter>
-            <DialogStackPrevious>Previous</DialogStackPrevious>
-            <DialogStackNext>Next</DialogStackNext>
-          </DialogStackFooter>
-        </DialogStackContent>
+          <DialogStackContent>
+            <DialogHeader>
+              <DialogStackTitle>Title</DialogStackTitle>
+              <DialogStackDescription>Description</DialogStackDescription>
+            </DialogHeader>
+            <p>Content</p>
+            <DialogStackFooter>
+              <DialogStackPrevious>Previous</DialogStackPrevious>
+              <DialogStackNext>Next</DialogStackNext>
+            </DialogStackFooter>
+          </DialogStackContent>
 
-        <DialogStackContent>
-          <DialogHeader>
-            <DialogStackTitle>Title</DialogStackTitle>
-            <DialogStackDescription>Description</DialogStackDescription>
-          </DialogHeader>
-          <p>Content</p>
-          <DialogStackFooter>
-            <DialogStackPrevious>Previous</DialogStackPrevious>
-            <DialogStackNext>Next</DialogStackNext>
-          </DialogStackFooter>
-        </DialogStackContent>
+          <DialogStackContent>
+            <DialogHeader>
+              <DialogStackTitle>Title</DialogStackTitle>
+              <DialogStackDescription>Description</DialogStackDescription>
+            </DialogHeader>
+            <p>Content</p>
+            <DialogStackFooter>
+              <DialogStackPrevious>Previous</DialogStackPrevious>
+              <DialogStackNext>Next</DialogStackNext>
+            </DialogStackFooter>
+          </DialogStackContent>
 
-        <DialogStackContent>
-          <DialogHeader>
-            <DialogStackTitle>Title</DialogStackTitle>
-            <DialogStackDescription>Description</DialogStackDescription>
-          </DialogHeader>
-          <p>Content</p>
-          <DialogStackFooter>
-            <DialogStackPrevious>Previous</DialogStackPrevious>
-            <DialogStackNext>Next</DialogStackNext>
-          </DialogStackFooter>
-        </DialogStackContent>
+          <DialogStackContent>
+            <DialogHeader>
+              <DialogStackTitle>Title</DialogStackTitle>
+              <DialogStackDescription>Description</DialogStackDescription>
+            </DialogHeader>
+            <p>Content</p>
+            <DialogStackFooter>
+              <DialogStackPrevious>Previous</DialogStackPrevious>
+              <DialogStackNext>Next</DialogStackNext>
+            </DialogStackFooter>
+          </DialogStackContent>
 
-        <DialogStackContent>
-          <DialogHeader>
-            <DialogStackTitle>Title</DialogStackTitle>
-            <DialogStackDescription>Description</DialogStackDescription>
-          </DialogHeader>
-          <p>Content</p>
-          <DialogStackFooter>
-            <DialogStackPrevious>Previous</DialogStackPrevious>
-            <DialogStackNext>Next</DialogStackNext>
-          </DialogStackFooter>
-        </DialogStackContent>
+          <DialogStackContent>
+            <DialogHeader>
+              <DialogStackTitle>Title</DialogStackTitle>
+              <DialogStackDescription>Description</DialogStackDescription>
+            </DialogHeader>
+            <p>Content</p>
+            <DialogStackFooter>
+              <DialogStackPrevious>Previous</DialogStackPrevious>
+              <DialogStackNext>Next</DialogStackNext>
+            </DialogStackFooter>
+          </DialogStackContent>
 
-        <DialogStackContent>
-          <DialogHeader>
-            <DialogStackTitle>Title</DialogStackTitle>
-            <DialogStackDescription>Description</DialogStackDescription>
-          </DialogHeader>
-          <p>Content</p>
-          <DialogStackFooter>
-            <DialogStackPrevious>Previous</DialogStackPrevious>
-          </DialogStackFooter>
-        </DialogStackContent>
-      </DialogStackBody>
-    </DialogStack>
-  );
-}
+          <DialogStackContent>
+            <DialogHeader>
+              <DialogStackTitle>Title</DialogStackTitle>
+              <DialogStackDescription>Description</DialogStackDescription>
+            </DialogHeader>
+            <p>Content</p>
+            <DialogStackFooter>
+              <DialogStackPrevious>Previous</DialogStackPrevious>
+            </DialogStackFooter>
+          </DialogStackContent>
+        </DialogStackBody>
+      </DialogStack>
+    </div>
+  </>
+);
+
+export default Home;
