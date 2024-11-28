c18af4aadd804e1af9f833f5c1a6660ece42b207
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-28 09:52:45 -0500

    Add fumadocs MDX plugins

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import createMdx from '@next/mdx';
+import {
+  type RehypeCodeOptions,
+  rehypeCode,
+  remarkGfm,
+  remarkHeading,
+} from 'fumadocs-core/mdx-plugins';
+import type { NextConfig } from 'next';
 
-const nextConfig = {
+const rehypeCodeOptions: RehypeCodeOptions = {
+  themes: {
+    light: 'catppuccin-mocha',
+    dark: 'catppuccin-mocha',
+  },
+};
+
+const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
@@ -8,6 +22,10 @@ const nextConfig = {
 
 const withMdx = createMdx({
   // Add markdown plugins here, as desired
+  options: {
+    rehypePlugins: [[rehypeCode, rehypeCodeOptions]],
+    remarkPlugins: [remarkGfm, remarkHeading],
+  },
 });
 
 // Merge MDX config with Next.js config
