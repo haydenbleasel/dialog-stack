3c153d66d4b81e3ac3ce9bb55fc62cb8ab0d7f53
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-02 09:53:12 -0500

    Update github-button.tsx

--- app/components/github-button.tsx
+++ app/components/github-button.tsx
@@ -29,9 +29,9 @@ export const GitHubButton = async (): Promise<ReactElement> => {
       <div className="flex h-full items-center">
         <div className="flex items-center gap-2 px-4 py-2">
           <Image src="/github.svg" alt="GitHub" width={16} height={16} />
-          <div className="hidden md:[display:unset]">GitHub</div>
+          <div>GitHub</div>
         </div>
-        <div className="hidden h-full w-px bg-border md:[display:unset]" />
+        <div className="h-full w-px bg-border" />
         <div className="px-4 py-2">
           <div>{stars}</div>
         </div>
